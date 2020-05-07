68beb43693eb1e030b5a42fddfe931cda46b4da2
unknown <git-commit-address>
2020-05-07 03:52:39 +0300

    componentleri öğrendik

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import User from "./components/User";
 
 function App() {
+
+const deneme = "Burak";
+const deneme2 = true;
   return (
     <div className="App">
-      <h1 style="text-align:center; ">SA</h1>
+
+     {1 + 1}
+     {"murat".toUpperCase()}
+     {deneme}
+      <div> 
+
+{
+deneme2 ? <p>Kayıtlı</p>
+: <p>Kayıtlı değil</p>
+}
+
+<h1>App Component</h1>
+<User/>
+
+      </div>
+
     </div>
   );
 }
